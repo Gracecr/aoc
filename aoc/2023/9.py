efe9a725489ec8c413cb9b3c24296bd89da7ea29
Chris Grace <cgrace.chris@gmail.com>
2023-12-09 23:56:42 -0500

    Easy day 9

---
+++ aoc/2023/9.py
@@ -0,0 +1,26 @@
+from aoc.util import *
+
+D = get_data().splitlines()
+# D = ["""10  13  16  21  30  45"""]
+
+def sub_list(l):
+    new_l = []
+    for a, b in zip(l, l[1:]):
+        new_l.append(b-a)
+    return new_l
+
+V = []
+for line in D:
+    data = [int(n) for n in line.split()]
+    hist = [list(data)]
+    while any(h != 0 for h in hist[-1]):
+        hist.append(sub_list(hist[-1]))
+    
+    val = 0
+    for r in hist[::-1]:
+        val = r[0] - val
+    
+    V.append(val)
+
+print(sum(V))
+submit(sum(V))
