165228e0ebbbd303f303694867bab122a26cf1eb
Chris Grace <cgrace.chris@gmail.com>
2023-12-13 01:34:54 -0500

    Buggy slow day 13. Not the worst

--- aoc/2023/12.py
+++ aoc/2023/12.py
@@ -10,35 +10,35 @@ D = get_data().splitlines()
 
 
 @functools.lru_cache
-def backtrack(s: str, pat: tuple[int], last: str = ""):
-    if not pat:
+def backtrack(s: str, groups: tuple[int], prev: str = ""):
+    if not groups:
         if "#" in s:
             return 0
         return 1
-    
-    if len(s) < pat[0] or s[0] == last == "#":
+
+    if len(s) < groups[0] or s[0] == prev == "#":
         return 0
 
     if s[0] == ".":
-        return backtrack(s[1:], pat, s[0])
+        return backtrack(s[1:], groups, s[0])
 
     if s[0] == "?":
-        return backtrack(s[1:], pat, ".") + backtrack("#" + s[1:], pat, last)
+        return backtrack(s[1:], groups, ".") + backtrack("#" + s[1:], groups, prev)
 
-    if any(True for char in s[:pat[0]] if char == "."):
+    if any(char == "." for char in s[:groups[0]]):
         return 0
-        
-    return backtrack(s[pat[0]:], pat[1:], "#")
+
+    return backtrack(s[groups[0]:], groups[1:], "#")
 
 acc = 0
 acc2 = 0
 for line in D:
-    springs, pattern = line.split()
-    pattern = [int(i) for i in pattern.split(",")]
+    springs, groups = line.split()
+    groups = [int(i) for i in groups.split(",")]
     p2_springs = "?".join([springs for _ in range(5)])
-    p2_pattern = pattern * 5
-    p1_result = backtrack(springs, tuple(pattern))
-    p2_result = backtrack(p2_springs, tuple(p2_pattern))
+    p2_groups = groups * 5
+    p1_result = backtrack(springs, tuple(groups))
+    p2_result = backtrack(p2_springs, tuple(p2_groups))
     acc += p1_result
     acc2 += p2_result
 
---
+++ aoc/2023/13.py
@@ -0,0 +1,72 @@
+from aoc.util import *
+
+D = [a.splitlines() for a in get_data().split("\n\n")]
+# D = [
+#     """#.##..##.
+# ..#.##.#.
+# ##......#
+# ##......#
+# ..#.##.#.
+# ..##..##.
+# #.#.##.#.""".splitlines(),
+# """#...##..#
+# #....#..#
+# ..##..###
+# #####.##.
+# #####.##.
+# ..##..###
+# #....#..#""".splitlines()]
+
+def is_ref(data: list[str], r):
+    for i in range(r):
+        if r + i >= len(data):
+            return True
+        if data[r - i - 1] != data[r + i]:
+            return False
+    return True
+
+cols = 0
+rows = 0
+for pattern in D:
+    orig_ref_line = [-1, -1]
+    for c in range(1, len(pattern[0])):
+        if is_ref(list(zip(*pattern)), c):
+            # cols += c
+            print("col", c)
+            orig_ref_line[0] = c
+            break
+    
+    for r in range(1, len(pattern)):
+        if is_ref(pattern, r):
+            print("row", r)
+            # rows += r
+            orig_ref_line[1] = r
+            break
+    
+
+    found_new_line = False
+    for r1, line in enumerate(pattern):
+        for c1, char in enumerate(line):
+            new_pattern = [list(line) for line in pattern]
+            if char == "#":
+                new_pattern[r1][c1] = "."
+            else:
+                new_pattern[r1][c1] = "#"
+            for c in range(1, len(new_pattern[0])):
+                if is_ref(list(zip(*new_pattern)), c) and c != orig_ref_line[0]:
+                    cols += c
+                    print("new col", c, "mod", (r1, c1))
+                    found_new_line = True
+            
+            for r in range(1, len(new_pattern)):
+                if is_ref(new_pattern, r) and r != orig_ref_line[1]:
+                    print("new row", r, "mod", (r1, c1))
+                    rows += r
+                    found_new_line = True
+            
+            if found_new_line:
+                break
+        if found_new_line:
+            break
+    
+submit(100 * rows + cols)
