e60b3b5f41d47c39f0983878acc1ba614b508eb4
Chris Grace <cgrace.chris@gmail.com>
2024-12-09 01:23:26 -0500

    Fix part 1, cleanup

--- aoc/2024/9.py
+++ aoc/2024/9.py
@@ -1,59 +1,65 @@
+from dataclasses import dataclass
+
 from aoc import util as u
 
+
+@dataclass
+class Block:
+    index: int
+    size: int
+
+
 D = u.get_data()
+if len(D) % 2 != 0:
+    # Assume free space size of 0 at the end if not specified
+    D += "0"
+
 SPACE = []
-FILLED_SPACES = []
-FREE_SPACES = []
+FILLED_SPACES: list[Block] = []
+FREE_SPACES: list[Block] = []
+
 
 p1 = 0
 p2 = 0
-id_num = 0
-if len(D) % 2 != 0:
-    D += "0"
-for size, free_size in zip(D[::2], D[1::2]):
-    FILLED_SPACES.append([len(SPACE), int(size)])
+
+for id_num, (size, free_size) in enumerate(zip(D[::2], D[1::2])):
+    FILLED_SPACES.append(Block(len(SPACE), int(size)))
     SPACE.extend([id_num] * int(size))
-    FREE_SPACES.append([len(SPACE), int(free_size)])
+    FREE_SPACES.append(Block(len(SPACE), int(free_size)))
     SPACE.extend(["."] * int(free_size))
-    id_num += 1
 
 for filled_space in FILLED_SPACES[::-1]:
     for free_space in FREE_SPACES:
-        if free_space[0] > filled_space[0]:
+        if free_space.index > filled_space.index:
             break
-        if filled_space[1] > free_space[1]:
+        if filled_space.size > free_space.size:
             continue
-        filled_space[0] = free_space[0]
-        free_space[1] -= filled_space[1]
-        free_space[0] += filled_space[1]
+        filled_space.index = free_space.index
+        free_space.size -= filled_space.size
+        free_space.index += filled_space.size
 
 
 for i, space in enumerate(FILLED_SPACES):
-    for j in range(space[1]):
-        p2 += (space[0] + j) * i
+    for j in range(space.size):
+        p2 += (space.index + j) * i
 
-# x = 0
-# y = len(SPACE) - 1
-# while y > x:
-#     if SPACE[y] == ".":
-#         y -= 1
-#         continue
-#     if SPACE[x] != ".":
-#         x += 1
-#         continue
-#     SPACE[x], SPACE[y] = SPACE[y], SPACE[x]
-#     x += 1
-#     y -= 1
+x = 0
+y = len(SPACE) - 1
+while y > x:
+    if SPACE[y] == ".":
+        y -= 1
+        continue
+    if SPACE[x] != ".":
+        x += 1
+        continue
+    SPACE[x], SPACE[y] = SPACE[y], SPACE[x]
+    x += 1
+    y -= 1
 
 
-# for i, v in enumerate(SPACE):
-#     if v != ".":
-#         p1 += v * i
-#         print(i, v, v * i)
+for i, v in enumerate(SPACE):
+    if v != ".":
+        p1 += v * i
 
-# print(p1)
-# for i, space in enumerate(FILLED_SPACES):
-#     for j in range(space[0], space[0] + space[1]):
-#         SPACE[j] = i
-# print("".join(str(s) for s in SPACE))
+print(p1)
 print(p2)
