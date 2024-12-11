01414d4a251b33c41dd12c4bf7f16f98534c79e4
Chris Grace <cgrace.chris@gmail.com>
2024-12-11 01:12:14 -0500

    Fix typo

--- aoc/2024/11.py
+++ aoc/2024/11.py
@@ -7,20 +7,20 @@ STONES = [int(s) for s in D.split()]
 
 
 @functools.lru_cache(maxsize=100000)
-def proccess_stone(stone: int, blinks: int) -> int:
+def process_stone(stone: int, blinks: int) -> int:
     if blinks == 0:
         return 1
 
     if stone == 0:
-        return proccess_stone(1, blinks - 1)
+        return process_stone(1, blinks - 1)
 
     if len(str(stone)) % 2 == 0:
         left = int(str(stone)[: len(str(stone)) // 2])
         right = int(str(stone)[len(str(stone)) // 2 :])
-        return proccess_stone(left, blinks - 1) + proccess_stone(right, blinks - 1)
+        return process_stone(left, blinks - 1) + process_stone(right, blinks - 1)
 
-    return proccess_stone(2024 * stone, blinks - 1)
+    return process_stone(2024 * stone, blinks - 1)
 
 
-print(sum(proccess_stone(stone, 25) for stone in STONES))
-print(sum(proccess_stone(stone, 75) for stone in STONES))
+print(sum(process_stone(stone, 25) for stone in STONES))
+print(sum(process_stone(stone, 75) for stone in STONES))
