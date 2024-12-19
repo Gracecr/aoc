f6fd79e3e2dde695650c88f887c4c282ce7f1a54
Chris Grace <cgrace.chris@gmail.com>
2024-12-18 20:37:32 -0500

    Day 18 cleanup

--- aoc/2024/18.py
+++ aoc/2024/18.py
@@ -6,28 +6,20 @@ V = u.get_vectors(2)
 R = 71
 C = 71
 B = ((0, R), (0, C))
-BLOCKS = set()
-TOTAL = 1024
-
-for i, d in enumerate(D):
-    if i >= TOTAL:
-        break
-    BLOCKS.add(tuple(map(int, d.split(","))))
+P1 = 1024
 
 
 for r in range(R):
     for c in range(C):
-        if (r, c) in BLOCKS:
-            continue
         for neigh in u.get_neighbors((r, c), V, B):
-            if neigh not in BLOCKS:
-                G.add_edge((r, c), neigh)
+            G.add_edge((r, c), neigh)
 
 
-print(u.nx.shortest_path_length(G, (0, 0), (R - 1, C - 1)))
-
-for d in D[TOTAL:]:
+for i, d in enumerate(D):
     G.remove_node(tuple(map(int, d.split(","))))
+    if i == P1:
+        print(u.nx.shortest_path_length(G, (0, 0), (R - 1, C - 1)))
+
     if not u.nx.has_path(G, (0, 0), (R - 1, C - 1)):
         print(d)
         break
